5f08c3527e146de5fb88fdbcac1a1e7af377b890
Ongchen Sherpa <fullstacksherpa@gmail.com>
2024-10-16 03:15:30 -0700

    CRUD operation done

--- API/src/controllers/products.controller.ts
+++ API/src/controllers/products.controller.ts
@@ -1,23 +1,76 @@
 import { Request, Response } from "express";
+import { db } from "@db/index";
+import { productsTable } from "@src/db/productsSchema";
+import { asyncHandler } from "@src/utils/asyncHandler";
+import { eq } from "drizzle-orm";
 
-const listProducts = (req: Request, res: Response) => {
-  res.send("getProduct list");
-};
+const listProducts = asyncHandler(async (req: Request, res: Response) => {
+  const products = await db.select().from(productsTable);
+  res.json(products);
+});
 
-const getProductsById = (req: Request, res: Response) => {
-  res.send("got product by Id");
-};
+const getProductsById = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const [product] = await db
+    .select()
+    .from(productsTable)
+    .where(eq(productsTable.id, Number(id)));
 
-const createProduct = (req: Request, res: Response) => {
-  res.send("Created new Product");
-};
+  if (!product) {
+    res.status(404).send({ message: "Product not found" });
+  } else {
+    res.json(product);
+  }
+});
 
-const updateProduct = (req: Request, res: Response) => {
-  res.send("updated product");
-};
+const createProduct = asyncHandler(async (req: Request, res: Response) => {
+  if (!req.body || !req.body.name || !req.body.price) {
+    return res.status(400).json({ message: "Name and price are required." });
+  }
 
-const deleteProduct = (req: Request, res: Response) => {
-  res.send("Product deleted successfully");
-};
+  if (typeof req.body.name !== "string" || typeof req.body.price !== "number") {
+    return res.status(400).json({
+      message: "Invalid data types. Name should be a string and price should be a number.",
+    });
+  }
+
+  const [product] = await db.insert(productsTable).values(req.body).returning();
+
+  res.status(201).json({ message: "Product created successfully", product });
+});
+
+const updateProduct = asyncHandler(async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  console.log(id);
+  const updatedFields = req.body;
+  if (!updateProduct) {
+    res.status(404).json({ message: `Please provide update product fields correctly` });
+  }
+
+  const [product] = await db
+    .update(productsTable)
+    .set(updatedFields)
+    .where(eq(productsTable.id, id))
+    .returning();
+
+  if (product) {
+    res.status(200).json({ message: "Product updated successfully", product: product });
+  } else {
+    res.status(404).json({ message: `Product with ID ${id} is not updated` });
+  }
+});
+
+const deleteProduct = asyncHandler(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const [deletedProduct] = await db
+    .delete(productsTable)
+    .where(eq(productsTable.id, Number(id)))
+    .returning();
+  if (deletedProduct) {
+    res.status(200).json({ message: "Product deleted successfully", product: deletedProduct });
+  } else {
+    res.status(404).json({ message: `Product with ID ${id} not found` });
+  }
+});
 
 export { listProducts, getProductsById, createProduct, updateProduct, deleteProduct };
--- API/src/index.ts
+++ API/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import productsRoutes from "@routes/products.routes";
+import { errorHandler } from "@middlewares/errorHandlerMiddleware";
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+
+//Centralized error handler at the end of all routes
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("Hello World?");
