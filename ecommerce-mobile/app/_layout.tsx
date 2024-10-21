528b8820f322f59bf6c96c76f0761158b6e1f1cd
Ongchen Sherpa <fullstacksherpa@gmail.com>
2024-10-21 00:10:30 -0700

    tansatack query and api integration

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,13 +1,21 @@
 import "@/global.css";
 import { Stack } from "expo-router";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <>
-      <GluestackUIProvider>
-        <Stack />
-      </GluestackUIProvider>
+      <QueryClientProvider client={queryClient}>
+        <GluestackUIProvider>
+          <Stack>
+            <Stack.Screen name="index" options={{ title: "shop" }} />
+            <Stack.Screen name="product/[id]" options={{ title: "Product" }} />
+          </Stack>
+        </GluestackUIProvider>
+      </QueryClientProvider>
     </>
   );
 }
--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,12 +1,26 @@
-import products from "../assets/products.json";
-import { FlatList } from "react-native";
+import { ActivityIndicator, FlatList } from "react-native";
 import ProductListItem from "@/components/ProductListItem";
+import { listProducts } from "@/api/product";
+import { useQuery } from "@tanstack/react-query";
+import { Text } from "@/components/ui/text";
 
 export default function HomeScreen() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: listProducts,
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Error fetching products</Text>;
+  }
   return (
     <FlatList
       numColumns={2}
-      data={products}
+      data={data}
       contentContainerClassName="gap-2"
       columnWrapperClassName="gap-2"
       renderItem={({ item }) => <ProductListItem product={item} />}
